aca006bd72d4a84569245f036765ac9694d939cd
Juan Ignacio Londra <londrajuan@gmail.com>
2022-11-27 01:14:55 -0300

    added virtualized table based on react window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import "../styles/estilos.css"
 import type { AppProps } from 'next/app'
-
+import 'normalize.css/normalize.css';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
