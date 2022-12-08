1f12dc361b57e3dbc747b60299abd17a74ae36f7
jualon92 <londrajuan@gmail.com>
2022-12-08 11:37:00 -0300

    changed reading on time test

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ export default function Home({ readings }: ReadingProps) {
 
 
   //if time registed on last reading does notg correspond with the actual time, readings are outdated
-  const areReadingsOnTime = () => "11" === "11"
+  const areReadingsOnTime = () => parseInt(getCurrentHour()).toString() === "11"
  
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
     null
