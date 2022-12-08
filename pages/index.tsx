ab957a1a67259c6617831f41a6e8d347834175be
Juan Ignacio Londra <londrajuan@gmail.com>
2022-12-07 23:02:10 -0300

    added last 24hour smoke and air pollutant warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,9 @@ import dbConnect from "../lib/mongoose";
 import Temperatura from "../models/Temperatura";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone"; // dependent on utc plugin
-import { FixedSizeList as List } from "react-window";
 
 import { TableVirtuoso } from "react-virtuoso";
-import React from "react";
+import React, { useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -15,47 +14,45 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Button, Popover, Typography } from "@mui/material";
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import HelpIcon from '@mui/icons-material/Help';
-import InfoSharpIcon from '@mui/icons-material/InfoSharp';
-import IconButton from '@mui/material/IconButton';
-export default function Home({ temperaturas, buildTimestamp }: any) {
+import { Button, Popover, Typography } from "@mui/material"; 
+import InfoSharpIcon from "@mui/icons-material/InfoSharp";
+import IconButton from "@mui/material/IconButton"; 
+
+interface Reading {
+  temperatura: string;
+  timestamp: string;
+  humidity: string;
+  isAirClean: boolean;
+  isSmokeFree: boolean;
+}
+
+interface ReadingProps {
+  readings: Reading[];
+}
+ 
+export default function Home({ readings }: ReadingProps) {
+  
+  
   const getCurrentHour = () => new Date().getHours().toLocaleString();
   const getLastReadingHour = () => {
-    
-    const lastHourNumber = temperaturas[0].timestamp.slice(0, 2);
-  
-    if (lastHourNumber.substring(0,1) === "0"){
-  
-      return lastHourNumber.replace(0, "")
-     
+    const lastHourNumber = readings[0].timestamp.slice(0, 2);
+
+    if (lastHourNumber.substring(0, 1) === "0") {
+      return lastHourNumber.replace("0", "");
     }
-    return lastHourNumber
-  }
+    return lastHourNumber;
+  };
 
   //if time registed on last reading does not correspond with the actual time, readings are outdated
-  const areReadingsOnTime = () => getCurrentHour() === getLastReadingHour() || parseInt(getCurrentHour()) ===  parseInt(getLastReadingHour()) - 1 ;
-
-  const getAirQualityStatus = () => {
-    if (temperaturas.airQuality < 33) {
-      return "Excellent";
-    }
-    if (temperaturas.airQuality > 34 && temperaturas.airQuality < 66) {
-      return "Very Good";
-    }
-    if (temperaturas.airQuality > 66 && temperaturas.airQuality < 100) {
-      return "Fair";
-    }
-    if (temperaturas.airQuality > 100 && temperaturas.airQuality < 149) {
-      return "Poor";
-    }
-  };
+  const areReadingsOnTime = () =>
+    getCurrentHour() === getLastReadingHour() ||
+    parseInt(getCurrentHour()) === parseInt(getLastReadingHour()) - 1;
 
   const isInvalidAirQualityReading = (reading: number) => reading > 3000;
 
-
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -66,7 +63,7 @@ export default function Home({ temperaturas, buildTimestamp }: any) {
   };
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const id = open ? "simple-popover" : undefined;
 
   const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
     //@ts-ignore
@@ -76,9 +73,40 @@ export default function Home({ temperaturas, buildTimestamp }: any) {
   const handlePopoverClose = () => {
     setAnchorEl(null);
   };
- 
 
+  const getTodayReading = () => {
+    //get all readings starting at 00:00
+    //get all temperatures with 00 hours.
+    const getFirstTemperatures = readings.find(
+      (temp: Reading) => temp.timestamp.slice(0, 2) === "24"
+    );
+  };
 
+  const getDayOfReading = (date: string) => {
+    const dateString = date.slice(11, 13);
+    const dateWithoutZero = parseInt(dateString);
+    return dateWithoutZero.toString();
+  };
+  const wasThereAnySmokeToday = () => {
+    return readings.some(
+      (reading) =>
+        !reading.isSmokeFree &&
+        getDayOfReading(reading.timestamp) === getToday()
+    );
+  };
+
+  const wasThereAnyToxicGasToday = () => {
+    return readings.some(
+      (reading) =>
+        !reading.isAirClean && getDayOfReading(reading.timestamp) === getToday()
+    );
+  };
+
+  const getToday = () => {
+    const today = new Date().getDate().toString().slice(0, 2);
+
+    return today;
+  };
   return (
     <div>
       <Head>
@@ -88,59 +116,87 @@ export default function Home({ temperaturas, buildTimestamp }: any) {
       </Head>
 
       <main>
-        <h1>ESPDUINO32 Room Readings <IconButton    
-         color="primary"  className={"BtnSpecs"}  aria-describedby={id}   onClick={handleClick}  >  <InfoSharpIcon fontSize="large" /> </IconButton>  </h1>
+        <h1>
+          ESPDUINO32 Room Readings{" "}
+          <IconButton
+            color="primary"
+            className={"BtnSpecs"}
+            aria-describedby={id}
+            onClick={handleClick}
+          >
+            {" "}
+            <InfoSharpIcon fontSize="large" />{" "}
+          </IconButton>{" "}
+        </h1>
 
         <div>
-      
-      <Popover
-
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <Typography sx={{ p: 2, maxWidth:"30rem"}}>
-        <div> Board: ESPDUINO32 </div>
-        <div> Temperature + Humidity:DHT11</div>
-        <div> Toxic Air Pollutants: MQ-135 </div> 
-        <div> Smoke detection: MQ-2 </div> 
-        <div> Timestamp: NTP  </div> 
-        <div> Arduino Libraries: 
-
-          Wifi.h, HTTPClient.h, ArduinoJson.h, NTPClient.h</div>
-         </Typography>
-      </Popover>
-    </div>
-
-        <div>
-          {areReadingsOnTime() ? (
-            <span> Readings are on schedule &#9989; </span>
-          ) : (
-            <span>
-               Readings are not up to date &#10060;
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            <Typography sx={{ p: 2, maxWidth: "30rem" }}>
+              <div> Board: ESPDUINO32 </div>
+              <div> Temperature + Humidity:DHT11</div>
+              <div> Toxic Air Pollutants: MQ-135 </div>
+              <div> Smoke detection: MQ-2 </div>
+              <div> Timestamp: NTP </div>
               <div>
-                {getLastReadingHour()}
-              Last reading was at {" "}
-             
-              {temperaturas[0].timestamp}  {" "}
+                {" "}
+                Arduino Libraries: Wifi.h, HTTPClient.h, ArduinoJson.h,
+                NTPClient.h
               </div>
-           
-            </span>
-          )}
+            </Typography>
+          </Popover>
         </div>
-            
+        <div>
+          <div>
+            {" "}
+            {wasThereAnySmokeToday() ? (
+              <span>Smoke has been detected at some point &#10060;</span>
+            ) : (
+              <span>No smoke detected as of today &#9989;</span>
+            )}{" "}
+          </div>
+          <div>
+            {" "}
+            {wasThereAnyToxicGasToday() ? (
+              <span>
+                Air Pollutants have been detected at some point &#10060;
+              </span>
+            ) : (
+              <span>
+                No Air Pollutant has been detected as of today &#9989;
+              </span>
+            )}{" "}
+          </div>
+          <div>
+            {areReadingsOnTime() ? (
+              <span> Readings are on schedule &#9989; </span>
+            ) : (
+              <span>
+                Readings are not up to date &#10060;
+                <div>
+                  {getLastReadingHour()}
+                  Last reading was at {readings[0].timestamp}{" "}
+                </div>
+              </span>
+            )}
+          </div>
+        </div>
+
         <TableVirtuoso
           style={{ height: 400, maxWidth: 660 }}
-          data={temperaturas}
+          data={readings}
           components={{
             // eslint-disable-next-line react/display-name
             Scroller: React.forwardRef((props, ref) => (
@@ -171,33 +227,32 @@ export default function Home({ temperaturas, buildTimestamp }: any) {
                 Humidity +/-5%
               </TableCell>
               <TableCell style={{ background: "white" }}>
-              Toxic Air Pollutants 
+                Toxic Air Pollutants
               </TableCell>
-              <TableCell style={{ background: "white" }}>
-              Smoke
-              </TableCell>
+              <TableCell style={{ background: "white" }}>Smoke</TableCell>
 
               <TableCell style={{ background: "white" }}>Timestamp</TableCell>
             </TableRow>
           )}
-          itemContent={(index, temp) => (
+          itemContent={(index, reading) => (
             <>
               <TableCell style={{ width: 150, background: "white" }}>
-                {temp.temperatura}
+                {reading.temperatura}
               </TableCell>
               <TableCell style={{ width: 150, background: "white" }}>
-                {temp.humidity}
+                {reading.humidity}
               </TableCell>
               <TableCell style={{ width: 150, background: "white" }}>
-                  {!temp.isAirClean ? "Air Pollutants DETECTED" : "NOT DETECTED"}
+                {!reading.isAirClean
+                  ? "Air Pollutants DETECTED"
+                  : "NOT DETECTED"}
               </TableCell>
               <TableCell style={{ background: "white", width: 100 }}>
-                {!temp.isSmokeFree ?  "SMOKE DETECTED" : "NO SMOKE"}
+                {!reading.isSmokeFree ? "SMOKE DETECTED" : "NO SMOKE"}
               </TableCell>
               <TableCell style={{ background: "white", width: 100 }}>
-                {temp.timestamp}
+                {reading.timestamp}
               </TableCell>
-             
             </>
           )}
         />
@@ -208,9 +263,9 @@ export default function Home({ temperaturas, buildTimestamp }: any) {
 
 export async function getStaticProps(context: any) {
   await dbConnect();
-  const temperaturasMongo = await Temperatura.find().lean();
+  const readingsMongo = await Temperatura.find().lean();
   // const temperatura  = temperaturasMongo.map( TempmongoDBEntry =>  TempmongoDBEntry.temperatura)
-  const temperaturas = JSON.parse(JSON.stringify(temperaturasMongo)).reverse();
+  const readings = JSON.parse(JSON.stringify(readingsMongo)).reverse();
 
   dayjs.extend(utc);
   dayjs.extend(timezone);
@@ -220,7 +275,6 @@ export async function getStaticProps(context: any) {
   const buildTimestamp = dayjs().format("HH:mm - DD/MM/YYYY");
 
   return {
-    props: { temperaturas, buildTimestamp }, // will be passed to the page component as props
-  
+    props: { readings, buildTimestamp }, // will be passed to the page component as props
   };
 }
